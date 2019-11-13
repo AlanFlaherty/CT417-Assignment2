709f3fa23b9922ce35106d8d913f2c63eb045b38
Alan Flaherty <a.flaherty13@nuigalway.ie>
2019-11-13 18:35:40 +0000

    Initial code base
    This is just the default maven archetype with .gitignore generated on
    github, manually added to the project before pushing to github.

---
+++ src/main/java/ie/nuigalway/ct417/alanflaherty/assignment2/App.java
@@ -0,0 +1,13 @@
+package ie.nuigalway.ct417.alanflaherty.assignment2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ie/nuigalway/ct417/alanflaherty/assignment2/AppTest.java
@@ -0,0 +1,20 @@
+package ie.nuigalway.ct417.alanflaherty.assignment2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
